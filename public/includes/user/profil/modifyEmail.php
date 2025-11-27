d67a2abae90af8c5a8a9383f682f7d5efc009acf
louise gratien <louise.gratien@gmail.com>
2025-11-27 17:16:38 +0100

    commit modify CRUD et index

--- public/includes/user/profil/modifyEmail.php
+++ public/includes/user/profil/modifyEmail.php
@@ -1,25 +1,46 @@
 <?php
-
+//verifier si formulaire non vide
 if (isset($_POST)&& !empty($_POST)){
+    //verifier si tous les champs sont remplie
     if (!empty($_POST['oldEmail']) && !empty($_POST['newEmail']) && !empty($_POST['pwd'])){
+        //securiser contre les injection de code
         $oldEmail = htmlspecialchars(trim($_POST['oldEmail']));
         $newEmail = htmlspecialchars(trim($_POST['newEmail']));
         $pwdVerify = htmlspecialchars(trim($_POST['pwd']));
+        //sortir les infos de la bdd
         $sql = "SELECT * FROM `user` WHERE `email` LIKE '$oldEmail'";
         $user = $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
-        $pwd  = $user['pwd'];
-        $_SESSION['email'] = $user['email'];
-        $_SESSION['id'] = $user['id'];
         $id =$_SESSION['id'];
-        var_dump($oldEmail===$_SESSION['email']);
-        var_dump(password_verify ($pwdVerify, $pwd ));
-            if ($oldEmail===$_SESSION['email'] && password_verify ($pwdVerify, $pwd )){
-                $modifyEmail = $pdo->prepare("UPDATE `user` SET `Email` = ? WHERE `id` = ?");
+        $newSql = "SELECT * FROM `user` WHERE `email` = ?";
+        $stmt = $pdo->prepare($newSql);
+        $stmt->execute([$newEmail]);
+        $newUser = $stmt->fetch(PDO::FETCH_ASSOC);
+        //verifier si l'utilisateur connait ses données de connexion
+        if ($oldEmail===$_SESSION['email'] && password_verify ($pwdVerify, $user['pwd'] )) {
+            //verifier si le email est libre
+            if ($newUser === false) {
+                //modifier le email
+                $modifyEmail = $pdo->prepare("UPDATE `user` SET `email` = ? WHERE `id` = ?");
                 $modifyEmail->execute([$newEmail, $id]);
-                var_dump($newEmail);
                 $_SESSION ['email'] = $newEmail;
+                //prevenir l'utilisateur
+                echo 'email modifié';
+
             }
+            else {
+                echo 'email deja pris';
+            }
+        }
+        else {
+            echo 'mot de passe ou email incorect';
+        }
     }
+    else{
+        echo 'veuillez remplir tout les champs';
+    }
+}
+else{
+    echo 'remplir le formulaire pour changer de email';
 }
 
 ?>
@@ -34,6 +55,5 @@ if (isset($_POST)&& !empty($_POST)){
     <label for="pwd">Mot de passe :</label><br>
     <input type="password" id="pwd" name="pwd" required><br><br>
 
-    <button type="submit">Modifier le Email</button>
+    <button type="submit">Modifier le email</button>
 </form>
-
