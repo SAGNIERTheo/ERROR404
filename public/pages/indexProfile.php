1b60cd986cb893d47650dd637ea47f32a1248741
SAGNIERTheo <theosagnier920@gmail.com>
2025-12-04 13:08:18 +0100

    Update indexProfile.php

--- public/pages/indexProfile.php
+++ public/pages/indexProfile.php
@@ -26,24 +26,4 @@ include_once './public/includes/nav.php';
 <a href="?page=logout">
     <button class="btn-profile-noir">Se déconnecter</button>
 </a>
-=======
-<section class="app-container">
-    <a href="?page=modifyProfil">
-        <button class="btn-profile">Modifier mon profil</button>
-    </a>
-    <a href="?page=alerts">
-        <button class="btn-profile">Notifications</button>
-    </a>
-    <a href="?page=#">
-        <button class="btn-profile">Mes dépenses</button>
-    </a>
-    <a href="?page=#">
-        <button class="btn-profile">Mes aniens évènements</button>
-    </a>
-    <a href="?page=organigrammeBDE">
-        <button>Organigramme BDE</button>
-    </a>
-    <a href="?page=logout">
-        <button class="btn-profile">Se déconnecter</button>
-    </a>
 </section>
